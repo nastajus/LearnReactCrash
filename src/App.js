1fa015cf34581af99bf94f7b8e0994016342bf5d
Ian Nastajus <ian.nastajus@gmail.com>
2018-02-06 00:19:05 -0500

    Component made for Project, bit of data passing going on

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
+import Projects from './Components/Projects'
 import './App.css';
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      projects: [
+        {
+          title: 'Business Website',
+          category: 'Web Design'
+        },
+        {
+          title: 'Social App',
+          category: 'Mobile Development'
+        },
+        {
+          title: 'Ecommerce Shopping',
+          category: 'Web Development'
+        }
+      ]
+    }
+  }
+
   render() {
     return (
       <div className="App">
         My App
+        //<Projects test="hello world"/>
+        <Projects projects={this.state.projects}/>
       </div>
     );
   }
