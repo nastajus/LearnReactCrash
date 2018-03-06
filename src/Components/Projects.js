ba38c3cc738c48edf640fd32b8f0bf01d1ffefc0
Ian Nastajus <ian.nastajus@gmail.com>
2018-03-06 16:04:56 -0500

    IDEA workspace.xml file update, with code formatting JSX update

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import ProjectItem from './ProjectItem'
 
 class Projects extends Component {
   render() {
     let projectItems;
     if (this.props.projects) {
-      projectItems = this.props.projects.map(project=>{
+      projectItems = this.props.projects.map(project => {
         //console.log(project);
         return (
-          <ProjectItem key={project.title} project={project} />
+          <ProjectItem key={project.title} project={project}/>
         );
       });
     }
